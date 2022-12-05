ed08d43549461a114392128d1c8bd6993fac5de5
Donci31 <szigetidonat@gmail.com>
2022-12-06 00:53:42 +0100

    download script

---
+++ download.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+
+
+os.makedirs('2019', exist_ok=True)
+os.makedirs('2020', exist_ok=True)
+os.makedirs('ossz', exist_ok=True)
+
+for year in range(2019, 2021):
+    for month in range(1, 13):
+        (
+            pd
+            .read_parquet(f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02d}.parquet')
+            .to_parquet(f'{year}/{year}-{month:02d}.parquet')
+        )
+        
